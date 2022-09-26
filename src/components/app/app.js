4966246a101559f7da513f339da17927cedb2271
Aleksandr Sykhover <Biotman@mail.ru>
2022-09-26 14:13:21 +0300

    Lesson 138 work filters

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,7 @@ class App extends Component {
                 {name: 'John V.', salary: 2200, increase: true, rise: false, id: 3}
             ],
             term: '', 
-            filter: ''
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -71,19 +71,33 @@ class App extends Component {
         this.setState({term});
     }
 
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise': 
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+                default:
+                    return items;
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
 
     render() {
         const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
-        const visibleData = this.searchEmp(data, term);
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter)
         
         return (
             <div className="app">
             <AppInfo employees={employees} increased={increased}/>
             <div className="search-panel">
                 <SearchPanel onUpdateSearch ={this.onUpdateSearch}/>
-                <AppFilter/>
+                <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
             </div>
             <EmloyersList 
                 data={visibleData}
